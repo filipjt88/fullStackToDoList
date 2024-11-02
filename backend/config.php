f3a7bb934f62a147b51a1856a3bc33b197cf05ba
filipjt88 <filip88ks@gmail.com>
2024-11-02 16:11:49 +0100

    save

--- backend/config.php
+++ backend/config.php
@@ -5,10 +5,10 @@
 $host = 'localhost';
 $db_name = 'todo_app';
 $user = 'root';
-$password = '';
+$pass = '';
 
 try {
-    $pdo = new PDO("mysql:host=$host;$db_name=$db_name", $user, $password);
+    $pdo = new PDO("mysql:host=$host;db_name=$db_name", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $error) {
     die('Connection failed: ' . $error->getMessage());
